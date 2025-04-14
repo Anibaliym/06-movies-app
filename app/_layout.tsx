5a061f99e38e654f91a6988aa0699f22e08dedcc
Anibal Yañez <Anibaliym@gmail.com>
2025-04-14 13:12:49 -0400

    se incorpora configuración de nativewind con errores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import "./global.css"
+
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-2xl font-bold">Hola desde NativeWind 🚀</Text>
     </View>
   )
 }
